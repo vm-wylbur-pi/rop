b06cc2ceb73c2935c962276f709a177b767a2114
Patrick Ball <pball@hrdag.org>
2019-08-06 15:50:35 -0700

    adding art.sh

---
+++ art.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+ROPPATH=$HOME/projects/personal/rop
+
+python3 $ROPPATH/art.py \
+	voronoi.py \
+	plasma.py \
+	spill.py \
+	ball.py \
+	bilinear.py \
+	block.py \
+	densefire.py \
+	downbilin.py \
+	lissajous.py \
+	plasma.py \
+	rainbow.py \
+	rise.py \
+	sparsefire.py \
+	sweep.py \
+	voronoi.py \
+	blobs.py \
+	bounce.py \
+	candy.py \
+	cascade.py \
+	curl.py \
+	grid.py \
+	mandel.py \
+	psyblobs.py \
+	rain.py \
+	rgbplasma.py \
+	sine.py
+
